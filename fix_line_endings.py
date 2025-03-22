9c40508a063eff4180aaf2066826668e68e8a60d
Kieran Finger <kieran.finger@gmail.com>
2025-03-21 23:31:25 -0500

    removed all files to keep locally, reset

--- fix_line_endings.py
+++
@@ -1,6 +0,0 @@
-# Fix line endings
-with open("data/nodes.csv", "r", newline="") as infile, open("nodes_fixed.csv", "w", newline="\n") as outfile:
-    for line in infile:
-        outfile.write(line.replace("\r\n", "\n"))
-
-print("Fixed CSV saved as nodes_fixed.csv")
